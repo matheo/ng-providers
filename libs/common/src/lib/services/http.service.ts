9c06a37b4e81f73dfe9de07ada12349c0cee5979
Mateo Tibaquira <nestormateo@gmail.com>
2020-05-02 18:54:26 -0500

    feat(common): add API_URL_TOKEN token

--- libs/common/src/lib/services/http.service.ts
+++ libs/common/src/lib/services/http.service.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHandler } from '@angular/common/http';
+import { API_URL_TOKEN } from '../tokens/api.token';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService extends HttpClient {
-  constructor(httpHandler: HttpHandler) {
+  constructor(
+    httpHandler: HttpHandler,
+    @Inject(API_URL_TOKEN) private apiUrl: string
+  ) {
     super(httpHandler);
   }
 }
