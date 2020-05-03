6c5cb2b11b6c003d3358d19b40739f02d6e82fd6
Mateo Tibaquira <nestormateo@gmail.com>
2020-05-02 20:11:16 -0500

    feat(website) : added styles to the main component

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MyCommonModule } from '@matheo/common';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
     MyCommonModule.forRoot({
       apiUrl: '/root',
     }),
