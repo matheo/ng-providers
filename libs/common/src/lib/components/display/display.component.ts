a70d13fbf551f726f7ba43715de85717c1f0a73a
Mateo Tibaquira <nestormateo@gmail.com>
2020-05-03 17:44:23 -0500

    fix(common): OnPush change detection for the display component

--- libs/common/src/lib/components/display/display.component.ts
+++ libs/common/src/lib/components/display/display.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpService } from '../../services/http.service';
 import { TreeShakeableService } from '../../services/tree-shakeable.service';
 
 @Component({
   selector: 'common-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
+  styleUrls: ['./display.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DisplayComponent implements OnInit {
   name = 'Loading...'
 
   constructor(
+    private cdr: ChangeDetectorRef,
     public http: HttpService,
     public shakeable: TreeShakeableService,
   ) { }
@@ -18,6 +20,7 @@ export class DisplayComponent implements OnInit {
   ngOnInit(): void {
     this.http.name().subscribe(data => {
       this.name = data['name'];
+      this.cdr.markForCheck();
     })
   }
 }
