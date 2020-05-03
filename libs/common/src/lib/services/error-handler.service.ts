cd3db4f1cff0c8704ee8d4cd9d919ef8b4757309
Mateo Tibaquira <nestormateo@gmail.com>
2020-05-03 17:20:24 -0500

    feat(common): add ERROR_RETRY_TOKEN to configure ErrorHandlerService

--- libs/common/src/lib/services/error-handler.service.ts
+++ libs/common/src/lib/services/error-handler.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { API_URL_TOKEN } from '../tokens/api.token';
+import { ERROR_RETRY_TOKEN } from '../tokens/error.token';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -9,12 +10,13 @@ export class ErrorHandlerService {
   i: number;
 
   constructor(
-    @Inject(API_URL_TOKEN) private apiUrl: string
+    @Inject(API_URL_TOKEN) private apiUrl: string,
+    @Inject(ERROR_RETRY_TOKEN) private errorRetry: string,
   ) {
     this.i = ErrorHandlerService.count;
     ErrorHandlerService.count++;
 
-    console.log(`new ErrorHandlerService(${this.apiUrl}) w/ ${this.i}`);
+    console.log(`new ErrorHandlerService(${this.i})(${this.apiUrl}) w/ ${this.errorRetry}`);
   }
 
   getResult(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
---
+++ libs/common/src/lib/tokens/error.token.ts
@@ -0,0 +1,3 @@
+import { InjectionToken } from '@angular/core';
+
+export const ERROR_RETRY_TOKEN = new InjectionToken<string>('errorRetry');
