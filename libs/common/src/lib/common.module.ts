0880f48ec224925d21fe1e21ae9ab7eb309cf9c0
Mateo Tibaquira <nestormateo@gmail.com>
2020-10-05 17:47:25 -0500

    build: ng update @angular/cli @angular/core

--- libs/common/src/lib/common.module.ts
+++ libs/common/src/lib/common.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DisplayComponent } from './components/display/display.component';
@@ -22,56 +22,62 @@ import { ERROR_RETRY_TOKEN } from './tokens/error.token';
   ],
 })
 export class MyCommonModule {
-  static forRoot(config: { apiUrl: string, errorRetry?: string }) {
+  static forRoot(config: {
+    apiUrl: string;
+    errorRetry?: string;
+}): ModuleWithProviders<MyCommonModule> {
     return {
-      ngModule: MyCommonModule,
-      providers: [
-        HttpService,
-        ErrorHandlerService,
-        {
-          provide: API_URL_TOKEN,
-          useValue: config.apiUrl,
-        },
-        {
-          provide: ERROR_RETRY_TOKEN,
-          useValue: config.errorRetry || 'defaultRetry',
-        },
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: InterceptorService,
-          multi: true,
-        }
-      ]
-    };
-  }
-
-  static forChild(config: { apiUrl?: string, errorRetry?: string }) {
-    return {
-      ngModule: MyCommonModule,
-      providers: [
-        /**
-         * The services using the Token needs to be provided
-         * in the same level of the hierarchy tree to use it instead the parent one.
-         */
-        !config.apiUrl
-          ? []
-          : [
-              HttpService,
-              {
+        ngModule: MyCommonModule,
+        providers: [
+            HttpService,
+            ErrorHandlerService,
+            {
                 provide: API_URL_TOKEN,
                 useValue: config.apiUrl,
-              },
-            ],
-        !config.errorRetry
-          ? []
-          : [
-              ErrorHandlerService,
-              {
+            },
+            {
                 provide: ERROR_RETRY_TOKEN,
-                useValue: config.errorRetry,
-              }
-            ],
-      ]
-    }
-  }
+                useValue: config.errorRetry || 'defaultRetry',
+            },
+            {
+                provide: HTTP_INTERCEPTORS,
+                useClass: InterceptorService,
+                multi: true,
+            }
+        ]
+    };
+}
+
+  static forChild(config: {
+    apiUrl?: string;
+    errorRetry?: string;
+}): ModuleWithProviders<MyCommonModule> {
+    return {
+        ngModule: MyCommonModule,
+        providers: [
+            /**
+             * The services using the Token needs to be provided
+             * in the same level of the hierarchy tree to use it instead the parent one.
+             */
+            !config.apiUrl
+                ? []
+                : [
+                    HttpService,
+                    {
+                        provide: API_URL_TOKEN,
+                        useValue: config.apiUrl,
+                    },
+                ],
+            !config.errorRetry
+                ? []
+                : [
+                    ErrorHandlerService,
+                    {
+                        provide: ERROR_RETRY_TOKEN,
+                        useValue: config.errorRetry,
+                    }
+                ],
+        ]
+    };
+}
 }
